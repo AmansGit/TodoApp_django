71e21d99ddc3c2ba79aa011ade82cb0ae89bc086
AmansGit <amans1997a@gmail.com>
2022-01-03 23:31:37 +0530

    views added

--- Todo/todolist_app/views.py
+++ Todo/todolist_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def todolist(request):
+	context = {
+		"welcome": "Welcome to TodoList Page"
+	}
+	return render(request, 'todolist.html', context)
+
+def contact(request):
+	context = {
+		"welcome": "Welcome to Contact Page"
+	}
+	return render(request, 'contact.html', context)
+
+def about(request):
+	context = {
+		"welcome": "Welcome to About Page"
+	}
+	return render(request, 'about.html', context)
