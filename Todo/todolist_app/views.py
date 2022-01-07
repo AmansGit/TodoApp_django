92ac75724016e6d7fb93dc7a720ee9e6dbb0fd3b
AmansGit <amans1997a@gmail.com>
2022-01-07 17:34:32 +0530

    html pages added

--- Todo/todolist_app/views.py
+++ Todo/todolist_app/views.py
@@ -3,18 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def todolist(request):
 	context = {
-		"welcome": "Welcome to TodoList Page"
+		"welcome": "Welcome to TodoList Page",
+		"title": "Home"
 	}
 	return render(request, 'todolist.html', context)
 
 def contact(request):
 	context = {
-		"welcome": "Welcome to Contact Page"
+		"welcome": "Welcome to Contact Page",
+		"title": "Contact"
 	}
 	return render(request, 'contact.html', context)
 
 def about(request):
 	context = {
-		"welcome": "Welcome to About Page"
+		"welcome": "Welcome to About Page",
+		"title": "About"
 	}
 	return render(request, 'about.html', context)
