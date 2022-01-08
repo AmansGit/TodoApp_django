679c90483027bbcb457510ab886da7a9f64dcc23
AmansGit <amans1997a@gmail.com>
2022-01-08 23:19:22 +0530

    directory structure

--- Todo/todolist_app/urls.py
+++ Todo/todolist_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('todolist/', views.todolist, name='todolist'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    # path('todolist/', views.todolist, name='todolist'),
 ]
