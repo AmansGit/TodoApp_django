cfb4ad93c809f7987257983724e09179ec982499
AmansGit <amans1997a@gmail.com>
2022-01-09 23:44:42 +0530

    urls

--- Todo/todolist_app/urls.py
+++ Todo/todolist_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('todolist/', views.todolist, name='todolist'),
-    path('contact/', views.contact, name='contact'),
-    path('about/', views.about, name='about'),
+    path('todolist', views.todolist, name='todolist'),
+    path('contact', views.contact, name='contact'),
+    path('about', views.about, name='about'),
     # path('todolist/', views.todolist, name='todolist'),
 ]
