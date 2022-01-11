d418d1b8e10105fb6d5a6ea9660740865beb028e
AmansGit <amans1997a@gmail.com>
2022-01-11 23:54:16 +0530

    html pages

--- Todo/todolist_app/urls.py
+++ Todo/todolist_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('todolist', views.todolist, name='todolist'),
     path('contact', views.contact, name='contact'),
-    path('about', views.about, name='about'),
+    path('about-us', views.about, name='about'),
     # path('todolist/', views.todolist, name='todolist'),
 ]
